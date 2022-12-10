32c22f1da30d841bed78f966921705aed40fa8fe
Oskar2706 <alexvaraksa@bk.ru>
2022-12-10 13:12:33 +0300

    hw

---
+++ src/ATM.java
@@ -0,0 +1,25 @@
+public class ATM {
+    int bill1 = 20;
+    int bill2 = 50;
+    int bill3 = 100;
+    int addbill;
+
+    public void addATM() {
+        addbill+=addbill;
+    }
+
+    public void takeoffATM() {
+        addbill-=addbill;
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/group1/CreditCard.java
+++ src/group1/CreditCard.java
@@ -1,8 +1,45 @@
 package group1;
 
 public class CreditCard {
-    public static void main(String[] args) {
+    String numberScore;
+    int sum;
+
+    public void getAdd() {
+        sum += sum;
+    }
+
+    public void getTakeOff() {
+        sum -= sum;
+    }
+
+    CreditCard (String numberScore, int sum) {
+        this.numberScore = numberScore;
+        this.sum = sum;
 
     }
 
-}
+    public static void main(String[] args) {
+        CreditCard Card1 = new CreditCard("A23 456 78B", 120);
+        CreditCard Card2 = new CreditCard("C87 654 32D", 35);
+        CreditCard Card3 = new CreditCard("F67 456 23K", 90);
+
+        Card1.getAdd();
+        Card2.getAdd();
+        Card3.getTakeOff();
+
+        System.out.println(Card1);
+        System.out.println(Card2);
+        System.out.println(Card3);
+    }
+
+    @Override
+    public String toString() {
+        return "CreditCard{" +
+                "numberScore='" + numberScore + '\'' +
+                ", sum=" + sum +
+                '}';
+    }
+
+    }
+
+
