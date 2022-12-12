18fa197f2902b99756b12361354a362fc854de7f
Oskar2706 <alexvaraksa@bk.ru>
2022-12-12 14:24:30 +0300

    laptop1/2

--- src/ATM.java
+++ src/ATM.java
@@ -1,25 +1,46 @@
 public class ATM {
-    int bill1 = 20;
-    int bill2 = 50;
-    int bill3 = 100;
+    int bill20;
+    int bill50;
+    int bill100;
     int addbill;
+    int takeoffbill;
 
-    public void addATM() {
-        addbill+=addbill;
+    public ATM (int bill20, int bill50, int bill100) {
+        this.bill20 = bill20;
+        this.bill50 = bill50;
+        this.bill100 = bill100;
     }
 
-    public void takeoffATM() {
-        addbill-=addbill;
+    public void addATM(int bill20, int bill50, int  bill100) {
+        addbill = this.bill20+bill20+this.bill50+bill50+this.bill100+bill100;
+
     }
 
+    public void takeoffATM(int bill20, int bill50, int  bill100, boolean operation) {
+        takeoffbill = this.bill20-bill20-this.bill50-bill50-this.bill100-bill100;
 
-
-
-
-
-
+    }
 
     public static void main(String[] args) {
+        ATM machine = new ATM(10, 15, 20);
+        machine.addATM(4,5,8);
+        System.out.println(machine);
 
+        machine.takeoffATM(3,2,7,true);
+        System.out.println(machine);
+
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "ATM{" +
+                "bill20=" + bill20 +
+                ", bill50=" + bill50 +
+                ", bill100=" + bill100 +
+                ", addbill=" + addbill +
+                ", takeoffbill=" + takeoffbill +
+                '}';
     }
 }
---
+++ src/Laptop/Laptop.java
@@ -0,0 +1,95 @@
+package Laptop;
+
+public class Laptop {
+    int price;
+    String model;
+    RAM ram;
+    HDD hdd;
+
+    public Laptop() {
+        price = 1500;
+        model ="SAMSUNG";
+        this.ram = new RAM();
+        this.hdd = new HDD();
+    }
+
+    public Laptop(int price, String model, RAM ram, HDD hdd) {
+        this.price = price;
+        this.model = model;
+        this.ram = ram;
+        this.hdd = hdd;
+
+    }
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "price=" + price +
+                ", model='" + model + '\'' +
+                ", ram=" + ram +
+                ", hdd=" + hdd +
+                '}';
+    }
+    public static void main(String[] args) {
+        Laptop.RAM ram = new RAM("APP",256);
+        Laptop.HDD hdd=new HDD("DDR",256,"256");
+        Laptop laptop1 = new Laptop();
+        Laptop laptop2 = new Laptop(1200,"LENOVO",ram,hdd);
+
+
+        System.out.println(laptop1);
+        System.out.println(laptop2);}
+
+    static class RAM {
+        String name;
+        int volume;
+
+        public RAM(String name, int volume) {
+            this.name = name;
+            this.volume = volume;
+        }
+
+        public RAM() {
+            name = "DDR4";
+            volume =512;
+        }
+        void ram1(){
+
+        }
+        @Override
+        public String toString() {
+            return "RAM{" +
+                    "name='" + name + '\'' +
+                    ", volume=" + volume +
+                    '}';
+        }
+    }
+    public static class HDD {
+        String name;
+        int volume;
+        String externaltype;
+
+        public HDD(String name, int volume, String externaltype) {
+            this.name = name;
+            this.volume = volume;
+            this.externaltype = externaltype;
+
+        }
+
+        public HDD() {
+            name="DDR";
+            volume=1024;
+            externaltype="512";
+
+
+        }
+
+        @Override
+        public String toString() {
+            return "HDD{" +
+                    "name='" + name + '\'' +
+                    ", volume=" + volume +
+                    ", externaltype='" + externaltype + '\'' +
+                    '}';
+        }
+    }
+}
